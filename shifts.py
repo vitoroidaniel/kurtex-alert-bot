797bea0c34d1db64b7ad70d7a11f2b4a3062fdf5
Vitoroi Daniel <vitoroid2@gmail.com>
2026-03-23 10:27:30 -0400

    Update shifts.py

--- shifts.py
+++ shifts.py
@@ -16,6 +16,8 @@ ADMINS = {
     7769230456: {"name": "Andrei",  "username": "Andrei_Cr05"},
     7808593054: {"name": "Petru",   "username": "Petru S"},
     6054170642: {"name": "Ion",   "username": "Ion AH"},
+    8755804962: {"name": "Andrei",   "username": "Andrei"},
+    6855707802: {"name": "Mihai AH",   "username": "Mihai AH"},
 }
 
 ALL_IDS = list(ADMINS.keys())
